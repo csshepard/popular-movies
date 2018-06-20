c1060be220261df98bfddad4d570e72ebd40c5d9
Chris Shepard <csshepard@gmail.com>
2018-06-20 00:15:30 -0400

    WIP Popular Movies Part 2

--- app/src/main/java/com/example/chris/popularmovies/themoviedb/TheMovieDBService.java
+++ app/src/main/java/com/example/chris/popularmovies/themoviedb/TheMovieDBService.java
@@ -1,9 +1,12 @@
 package com.example.chris.popularmovies.themoviedb;
 
 import com.example.chris.popularmovies.themoviedb.model.MovieResultList;
+import com.example.chris.popularmovies.themoviedb.model.ReviewResultList;
+import com.example.chris.popularmovies.themoviedb.model.VideoResultList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface TheMovieDBService {
@@ -13,5 +16,11 @@ public interface TheMovieDBService {
 
     @GET("movie/top_rated")
     Call<MovieResultList> getTopRatedMovies(@Query("api_key") String apiKey, @Query("page") Integer page);
+
+    @GET("movie/{movie_id}/videos")
+    Call<VideoResultList> getMovieVideos(@Path("movie_id") Integer movieId, @Query("api_key") String apiKey);
+
+    @GET("movie/{movie_id}/reviews")
+    Call<ReviewResultList> getMovieReviews(@Path("movie_id") Integer movieId, @Query("api_key") String apiKey, @Query("page") Integer page);
 
 }
